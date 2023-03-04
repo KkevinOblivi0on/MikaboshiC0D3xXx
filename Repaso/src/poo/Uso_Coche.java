0337b6b6b385c13b6724c59a922cf351d689b48a
Estudiante USC <estudiante@GTACAD.USC>
2023-03-04 16:19:58 -0500

    mi cuarto commit

---
+++ Repaso/src/poo/Uso_Coche.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Coche Renault=new Coche();
+		
+		
+		Renault.ruedas=4;
+
+
+      System.out.println("Este coche tiene " + Renault.ruedas + "ruedas.");
+	}
+
+}
