8dfb5605c351ecd12bf4813498c219beeeef5c30
Joseph10389 <fabio.cuero00@usc.edu.co>
2023-03-25 16:19:47 -0500

    mi billon commit

---
+++ TallerSieteManejoDeErrores/src/ErrorLogicob.java
@@ -0,0 +1,23 @@
+
+public class ErrorLogicob {
+	
+	
+
+	public static void main(String[] args) {
+	    int[] numeros = {5, 10, 15, 20};
+	    int i = 0;
+	    while (numeros[i] != 15) {
+	        i++;
+	    }
+	    System.out.println("El índice del número 15 es: " + i);
+	}
+
+	//Se busca un indice 15 en array pero al no introducirse dentro creara un bucle infinito de y fallar. por ende hay un error logico :/
+	
+	
+	
+
+			
+			
+			
+}
